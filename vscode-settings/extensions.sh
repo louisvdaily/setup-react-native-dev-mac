0023f4056c546c9e661ae1433445a53a47515035
Louis Daily <developer@louisdaily.com>
2024-08-23 17:57:15 -0500

    feat(vscode): add script to install recommended VS Code extensions

---
+++ vscode-settings/extensions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ES7+ React/Redux/React-Native snippets
+code --install-extension dsznajder.es7-react-js-snippets
+
+# ESLint
+code --install-extension dbaeumer.vscode-eslint
+
+# Prettier - Code formatter
+code --install-extension esbenp.prettier-vscode
+
+# Code Spell Checker
+code --install-extension streetsidesoftware.code-spell-checker
+
+# React Native Snippet
+code --install-extension msjsdiag.vscode-react-native
+
+# Path Intellisense
+code --install-extension christian-kohler.path-intellisense
+
+# Image Preview
+code --install-extension kisstkondoros.vscode-gutter-preview
+
+# SVG Preview
+code --install-extension SimonSiefke.svg-preview
